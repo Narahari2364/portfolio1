be3ca29e1274422a43a0f83fae622824daa8a228
Narahari bheemaganapalli narasimha reddy <naraharibn2364@gmail.com>
2025-11-11 11:15:00 -0500

    Refactor CharacterModel component to remove loading logic and replace with Scene component

--- src/components/Character/index.tsx
+++ src/components/Character/index.tsx
@@ -1,31 +1,7 @@
-import { useEffect } from "react";
-import { useLoading } from "../../context/LoadingProvider";
-import { setProgress } from "../Loading";
+import Scene from "./Scene";
 
 const CharacterModel = () => {
-  const { setLoading } = useLoading();
-
-  useEffect(() => {
-    const progress = setProgress((value) => setLoading(value));
-    const img = new Image();
-    img.src = "/portrait.png";
-    img.onload = () => {
-      progress.loaded();
-    };
-    img.onerror = () => {
-      progress.loaded();
-    };
-  }, []);
-
-  return (
-    <div className="character-container">
-      <div className="character-model character-model-photo">
-        <div className="portrait-glow portrait-glow-warm"></div>
-        <div className="portrait-glow portrait-glow-cool"></div>
-        <img src="/portrait.png" alt="Narahari BN" className="portrait-image" />
-      </div>
-    </div>
-  );
+  return <Scene />;
 };
 
 export default CharacterModel;
